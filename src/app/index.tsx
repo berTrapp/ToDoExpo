f6a9be58b8cbea8a776efb28811406556a6e33ce
bertrapp <bernardo.trapp@gmail.com>
2025-08-13 12:26:40 -0300

    primeiro commit

---
+++ src/app/index.tsx
@@ -0,0 +1,10 @@
+import {View} from "react-native";
+import Title from "../components/title";
+
+export default function Index() {
+    return(
+        <View>
+            <Title>Hello World - CHILDREN</Title>
+        </View>
+    );
+}
